a192b361a1cf6c5c06aeace26acd2c5482cc7fc6
K Li <kyli.nju@gmail.com>
2021-03-23 23:03:30 -0400

    review code

--- lc211.java
+++ lc211.java
@@ -17,7 +17,7 @@ Implement the WordDictionary class:
 WordDictionary() Initializes the object.
 void addWord(word) Adds word to the data structure, it can be matched later.
 bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
- 
+
 
 Example:
 
@@ -36,7 +36,7 @@ wordDictionary.search("pad"); // return False
 wordDictionary.search("bad"); // return True
 wordDictionary.search(".ad"); // return True
 wordDictionary.search("b.."); // return True
- 
+
 
 Constraints:
 
@@ -50,44 +50,44 @@ At most 50000 calls will be made to addWord and search.
 
 class WordDictionary {
 
-    class Node{
-        String isEnd;
+    class Node{  // you can call it TrieNode
+        String isEnd;  // is* should be a boolean type, and you don't have to save the word string here, you only need a boolean value.
         Node[] children;
         Node(){
-        children = new Node[128];
+        children = new Node[128];  // indentation. hashmap is better because when size is small, only part of 128 chars are used.
         }
     }
     Node start;
     public WordDictionary() {
          start = new Node();
     }
-    
+
     public void addWord(String word) {
         Node head = start;
         for(int i = 0; i < word.length(); i++){
             int idx = word.charAt(i) -'a';
             if(head.children[idx] == null){
-               head.children[idx] = new Node(); 
+               head.children[idx] = new Node();
             }
             //System.out.println(Arrays.toString(head.children));
             head = head.children[idx];
-            
+
         }
-        head.isEnd = word;   
+        head.isEnd = word;
     }
-    
-    
+
+
     public boolean search(String word) {
-        
+
         return search(word, 0, start);
     }
     public boolean search(String word, int ind, Node cur){
         if(cur == null) return false;
-        
+
         if(ind == word.length()){
-            return cur.isEnd != null; 
+            return cur.isEnd != null;
         }
-        
+
         if(word.charAt(ind) == '.'){
             for(Node child : cur.children){
                 if(search(word, ind + 1, child)) return true;
@@ -98,3 +98,5 @@ class WordDictionary {
         return false;
     }
 }
+
+// List a few unit test cases after you finish the implementation.
